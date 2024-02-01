e10e2b3f974a55ec7b82de02275c22f7ccda3ac2
aliciailiesiu <aliciailiesiu@outlook.com>
2024-02-01 16:51:25 -0500

    did input speech

--- experiments/p5.speech-experiments/js/script.js
+++ experiments/p5.speech-experiments/js/script.js
@@ -8,24 +8,63 @@
 
 "use strict";
 
+const speechSynthesizer = new p5.Speech();
 
-// 1. Create a speech recognition object
-let speechRecognizer = new p5.SpeechRec();
-
-// Called at the start of the program
 function setup() {
-    // 2. Tell the program the function to call if the recognizer hears something
-    speechRecognizer.onResult = handleResult;
-    // 3. Tell the recognizer to start listening
-    speechRecognizer.start();
+    createCanvas(500,500);
+   
+    //synthesis settings
+    speechSynthesizer.setPitch(1);
+    speechSynthesizer.setRate(1);
+    speechSynthesizer.setVoice(`Google UK English Male`);
+
+    console.log(speechSynthesizer.listVoices());
+
 }
 
-// Called when the recognizer has heard something
-function handleResult() {
-    // 4. Check if there is definitely a result
-    if (speechRecognizer.resultValue === true) {
-        // 5. If there is, print it out in the console
-        console.log(speechRecognizer.resultString);
-        // (e.g. speechRecognizer.resultString has what was said inside it)
-    }
+function draw() {
+    background(212,102,157);
+   
 }
+
+function mousePressed() {
+    //say something
+    speechSynthesizer.speak(`hello bitch`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 1. Create a speech recognition object
+// let speechRecognizer = new p5.SpeechRec();
+
+// // Called at the start of the program
+// function setup() {
+//     // 2. Tell the program the function to call if the recognizer hears something
+//     speechRecognizer.onResult = handleResult;
+//     // 3. Tell the recognizer to start listening
+//     speechRecognizer.start();
+// }
+
+// // Called when the recognizer has heard something
+// function handleResult() {
+//     // 4. Check if there is definitely a result
+//     if (speechRecognizer.resultValue === true) {
+//         // 5. If there is, print it out in the console
+//         console.log(speechRecognizer.resultString);
+//         // (e.g. speechRecognizer.resultString has what was said inside it)
+//     }
+// }
