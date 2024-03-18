624d42ea2154bd59e245d8c6599a8d4c7c956a32
aliciailiesiu <aliciailiesiu@outlook.com>
2024-03-18 18:12:51 -0400

    finished experiment

--- experiments/game-engine/js/Play.js
+++ experiments/game-engine/js/Play.js
@@ -11,24 +11,74 @@ class Play extends Phaser.Scene {
   
         create() {
             
-            this.wall = this.add.sprite(100, 100, `wall`);
-            this.avatar = this.add.sprite(200, 200, `avatar`);
+            // this.wall = this.physics.add.sprite(100, 100, `wall`);
+            // this.wall.setImmovable(true);
+
+            this.walls = this.physics.add.group({
+                // All walls should use the wall image key
+                key: `wall`,
+                // Make all the walls created immovable
+                immovable: true,
+                // Create 16 walls
+                quantity: 20,
+              });
+              this.walls.children.each(function(wall) {
+                let x = Phaser.Math.Between(0, this.sys.canvas.width);
+                let y = Phaser.Math.Between(0, this.sys.canvas.height);
+                // Set the position of the current wall
+                wall.setPosition(x, y);
+                // Set the tint of the current wall
+                wall.setTint(`0xdd3333`);
+                // Note how we pass "this" as the second argument to .each() so that we can use the class'
+                // methods and properties if needed
+              }, this);
+
+            // this.collectable = this.physics.add.sprite(300, 300, `wall`);
+            // this.collectable.setTint(`0x3333dd`);
+
+            this.collectables = this.physics.add.group({
+                key: 'wall',
+                quantity: 70
+              });
+          
+              this.collectables.children.each(function(collectable) {
+                let x = Phaser.Math.Between(0, this.sys.canvas.width);
+                let y = Phaser.Math.Between(0, this.sys.canvas.height);
+                collectable.setPosition(x, y);
+                collectable.setTint(`0x3333dd`);
+              }, this);
+
+            this.avatar = this.physics.add.sprite(200, 200, `avatar`);
+            this.avatar.setTint(`0xdd3333`);
+
 
             this.createAnimations();
 
-            this.avatar.play(`avatar-moving`);
-          
+            // this.avatar.setVelocityX(100);
+            this.avatar.play(`avatar-idle`);
+            this.avatar.setCollideWorldBounds(true);
+
+            this.physics.add.collider(this.avatar, this.walls);
+            this.physics.add.overlap(this.avatar, this.collectables, this.collectItem, null, this);
 
 
+            this.cursors = this.input.keyboard.createCursorKeys();
           }
     
   
     // NOTE: The update() method is a lot like the p5.js draw() function, it's called once
     // every animation frame
     update() {
-    //   // Let's put in another message...
-    //   console.log("Play scene updated!");
+        this.handleInput();
     }
+
+
+    collectItem(avatar, item) {
+        // NOTE: We'll keep it simple by just removing the collectable from the scene
+        // using its .destroy() method!
+        item.destroy();
+      }
+
 
     createAnimations() {
         this.anims.create({
@@ -50,5 +100,46 @@ class Play extends Phaser.Scene {
             frameRate: 24,
             repeat: -1
           });
+    }
+
+
+    handleInput() {
+        // NOTE: We can now check which keys are pressed and set the velocity of our
+        // avatar sprite accordingly.
+        // EXAMPLE: https://phaser.io/examples/v3/view/input/keyboard/cursor-keys
+        if (this.cursors.left.isDown) {
+          this.avatar.setVelocityX(-300);
+        }
+        else if (this.cursors.right.isDown) {
+          this.avatar.setVelocityX(300);
+        }
+        else {
+          // If neither left or right are pressed, stop moving on x
+          this.avatar.setVelocityX(0);
+        }
+    
+        if (this.cursors.up.isDown) {
+          this.avatar.setVelocityY(-300);
+        }
+        else if (this.cursors.down.isDown) {
+          this.avatar.setVelocityY(300);
+        }
+        else {
+          // If neither up or down are pressed, stop moving on y
+          this.avatar.setVelocityY(0);
+        }
+
+         // If either x or y velocity isn't zero, that the avatar is moving
+    if (this.avatar.body.velocity.x !== 0 || this.avatar.body.velocity.y !== 0) {
+        // NOTE: That we include a second argument of "true" to tell the animation system
+        // to ignore this instruction if the animation is already playing. This avoids
+        // having the animation get constantly interrupted as the player moves
+        // DOCS: https://newdocs.phaser.io/docs/3.53.1/Phaser.GameObjects.Sprite#play
+        this.avatar.play(`avatar-moving`, true);
+      }
+      // Otherwise it's not moving
+      else {
+        this.avatar.play(`avatar-idle`, true);
+      }
     }
   }
